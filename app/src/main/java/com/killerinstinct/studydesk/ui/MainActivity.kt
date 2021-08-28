9161300de1b7215f1e46566b59ed597e3b913f87
Vibin <vibin.cr22@gmail.com>
2021-08-28 08:50:44 +0530

    Packages setup

---
+++ app/src/main/java/com/killerinstinct/studydesk/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.killerinstinct.studydesk.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.killerinstinct.studydesk.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
