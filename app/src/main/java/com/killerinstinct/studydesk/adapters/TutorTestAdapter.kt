bcc97a0fb5f5db811d4ef0b301a7f7fb1d122038
SanthiyaM-code <santhiya10082001@gmail.com>
2021-08-29 22:43:13 +0530

    tutorTestAdapter added

---
+++ app/src/main/java/com/killerinstinct/studydesk/adapters/TutorTestAdapter.kt
@@ -0,0 +1,48 @@
+package com.killerinstinct.studydesk.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.killerinstinct.studydesk.R
+import com.killerinstinct.studydesk.data.models.Test
+
+class TutorTestAdapter (
+
+    private var list:List<Test>,
+    private val context: Context
+): RecyclerView.Adapter<TutorTestAdapter.TutorTestViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TutorTestAdapter.TutorTestViewHolder {
+
+        return TutorTestViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.test_card_view, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TutorTestAdapter.TutorTestViewHolder, position: Int) {
+        holder.name.text = list[position].title
+        holder.sub_name.text = list[position].subject
+        holder.dueDate.text = list[position].date
+        holder.dueTime.text = list[position].time
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class TutorTestViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name = view.findViewById<TextView>(R.id.staff_name)
+        val sub_name = view.findViewById<TextView>(R.id.class_name)
+        val dueDate= view.findViewById<TextView>(R.id.subject_name)
+        val dueTime = view.findViewById<TextView>(R.id.studnt_count)
+
+    }
+}
