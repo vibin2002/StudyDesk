ed9389be2dc8d5f803e3c33f30756124f0de5cb7
SanthiyaM-code <santhiya10082001@gmail.com>
2021-08-30 16:06:42 +0530

    ui done for add test and add assingment

--- app/src/main/java/com/killerinstinct/studydesk/ui/tutor/drawer/home/AddAssignmentFragment.kt
+++ app/src/main/java/com/killerinstinct/studydesk/ui/tutor/drawer/home/AddAssignmentFragment.kt
@@ -1,18 +1,37 @@
 package com.killerinstinct.studydesk.ui.tutor.drawer.home
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.fragment.app.Fragment
 import com.killerinstinct.studydesk.R
 import com.killerinstinct.studydesk.databinding.FragmentAddAssignmentBinding
 import com.killerinstinct.studydesk.ui.tutor.TutorMainViewModel
+import java.util.*
 
-class AddAssignmentFragment : Fragment(R.layout.fragment_add_assignment) {
+class AddAssignmentFragment : Fragment(R.layout.fragment_add_assignment)
+//    DatePickerDialog.OnDateSetListener,
+//    TimePickerDialog.OnTimeSetListener
+{
+
+//    var day=0
+//    var month=0
+//    var year=0
+//    var hour=0
+//    var minute=0
+//    var Savday=0
+//    var Savmonth=0
+//    var Savyear=0
+//    var Savhour=0
+//    var Savminute=0
+
 
     private lateinit var binding: FragmentAddAssignmentBinding
     private val viewModel: TutorMainViewModel by activityViewModels()
@@ -38,6 +57,46 @@ class AddAssignmentFragment : Fragment(R.layout.fragment_add_assignment) {
 
         }
 
+
+//        binding.dueDate.setOnClickListener{
+//            getDateTime()
+//            DatePickerDialog(requireActivity(),this,year,month,day)
+//        }
+//        binding.dueTime.setOnClickListener{
+//            getDateTime()
+//            TimePickerDialog(requireActivity(),this,hour,minute,true)
+//        }
     }
+
+//    private  fun getDateTime()
+//    {
+//        val cal:Calendar=Calendar.getInstance()
+//        day=cal.get(Calendar.DAY_OF_MONTH)
+//        month=cal.get(Calendar.MONTH)
+//        year=cal.get(Calendar.YEAR)
+//        hour=cal.get(Calendar.HOUR)
+//        minute=cal.get(Calendar.MINUTE)
+//
+//    }
+
+//    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+//        Savday=dayOfMonth
+//        Savmonth=month
+//        Savyear=year
+//
+//        getDateTime()
+//
+//        binding.dueDate.text="$Savday-$Savmonth-$Savyear"
+//    }
+
+//    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+//
+//        Savhour=hourOfDay
+//        Savminute=minute
+//
+//        getDateTime()
+//
+//        binding.dueTime.text="$Savhour:$Savminute"
+//    }
 
 }
--- app/src/main/java/com/killerinstinct/studydesk/ui/tutor/drawer/home/AddTestFragment.kt
+++ app/src/main/java/com/killerinstinct/studydesk/ui/tutor/drawer/home/AddTestFragment.kt
@@ -1,5 +1,7 @@
 package com.killerinstinct.studydesk.ui.tutor.drawer.home
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,11 +10,27 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import android.widget.DatePicker
+import android.widget.TimePicker
 import com.killerinstinct.studydesk.R
 import com.killerinstinct.studydesk.databinding.FragmentAddTestBinding
 import com.killerinstinct.studydesk.ui.tutor.TutorMainViewModel
+import java.util.*
 
-class AddTestFragment : Fragment(R.layout.fragment_add_test) {
+class AddTestFragment : Fragment(R.layout.fragment_add_test)
+//    DatePickerDialog.OnDateSetListener,
+//    TimePickerDialog.OnTimeSetListener
+{
+//    var day=0
+//    var month=0
+//    var year=0
+//    var hour=0
+//    var minute=0
+//    var Savday=0
+//    var Savmonth=0
+//    var Savyear=0
+//    var Savhour=0
+//    var Savminute=0
 
     private lateinit var binding: FragmentAddTestBinding
     private val viewModel: TutorMainViewModel by activityViewModels()
@@ -20,6 +38,23 @@ class AddTestFragment : Fragment(R.layout.fragment_add_test) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentAddTestBinding.bind(view)
+//        binding.dueDate.setOnClickListener{
+//            getDateTime()
+//            DatePickerDialog(requireActivity(),this,year,month,day)
+//        }
+//        binding.dueTime.setOnClickListener{
+//            getDateTime()
+//            TimePickerDialog(requireActivity(),this,hour,minute,true)
+//        }
+//    }
+//    private  fun getDateTime()
+//    {
+//        val cal: Calendar = Calendar.getInstance()
+//        day=cal.get(Calendar.DAY_OF_MONTH)
+//        month=cal.get(Calendar.MONTH)
+//        year=cal.get(Calendar.YEAR)
+//        hour=cal.get(Calendar.HOUR)
+//        minute=cal.get(Calendar.MINUTE)
 
         val navController = findNavController()
         viewModel.addTest(
@@ -36,10 +71,24 @@ class AddTestFragment : Fragment(R.layout.fragment_add_test) {
             } else {
                 Toast.makeText(requireActivity(), "Test not added", Toast.LENGTH_SHORT).show()
             }
-
         }
 
     }
+//    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+//        Savday=dayOfMonth
+//        Savmonth=month
+//        Savyear=year
+//
+//        binding.dueDate.text="$Savday-$Savmonth-$Savyear"
+//    }
+
+//    }
+//    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+//        Savhour=hourOfDay
+//        Savminute=minute
+//
+//        binding.dueTime.text="$Savhour:$Savminute"
+//    }
 
 
 }
